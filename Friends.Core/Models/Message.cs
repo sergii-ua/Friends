8af3139877ae8a830ab2f5e44786c36a1ede03bf
sergii-ua <sergii-ua@ukr.net>
2022-01-02 20:10:45 +0200

    Initial commit

---
+++ Friends.Core/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Friends.Core.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string MessageBody { get; set; }
+        public int MessageFromId { get; set; }
+        public int MessageToId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Friends.Core/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Friends.Core.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Message> Messages { get; set; }
+    }
+}
