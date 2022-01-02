eb0fc9e3c36ea857e629f34e374b58bac397f251
sergii-ua <sergii-ua@ukr.net>
2022-01-02 20:21:59 +0200

    Core - models defined

--- Friends.Core/Models/User.cs
+++ Friends.Core/Models/User.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Friends.Core.Models
 {
     public class User
     {
+        public User()
+        {
+            Messages = new Collection<Message>();
+        }
         public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
