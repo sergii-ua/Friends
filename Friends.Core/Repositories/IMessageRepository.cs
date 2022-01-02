b658821b8a6181290d75677c655e3ca5323b96dd
sergii-ua <sergii-ua@ukr.net>
2022-01-02 20:40:16 +0200

    adding repository

---
+++ Friends.Core/Repositories/IMessageRepository.cs
@@ -0,0 +1,15 @@
+﻿using Friends.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friends.Core.Repositories
+{
+    public interface IMessageRepository : IRepository<Message>
+    {
+        Task<IEnumerable<Message>> GetAllWithUserByIdAsync();
+        Task<Message> GetWithUserByIdAsync();
+        Task<IEnumerable<Message>> GetAllWithUserByUserIdAsync();
+    }
+}
---
+++ Friends.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Friends.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Friends.Core.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<IEnumerable<User>> GetAllWithMessagesAsync();
+        Task<User> GetWithMessagesByIdAsync();
+    }
+}
